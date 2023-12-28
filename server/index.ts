3417e51c9cf867ea647fe3e3a74ddbb4b6bc886b
ChrisLubin <chrisx700@gmail.com>
2023-12-28 13:22:40 -0500

    Created the hostGame and joinGame API routes.

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
-import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
+import express, { Express, Request, Response } from 'express';
+import { generateRandomDigit } from './utilities';
 
 const app: Express = express();
 const port = 3000;
@@ -13,6 +14,18 @@ app.use(cors(corsOptions));
 app.get('/test', (req: Request, res: Response) => {
   console.log('ENDPOINT IS REACHED');
   res.send({ msg: 'This is the beginning of the card battle' });
+});
+
+app.get('/hostGame', (req: Request, res: Response) => {
+  const randomPin = `${generateRandomDigit()}${generateRandomDigit()}${generateRandomDigit()}${generateRandomDigit()}`;
+  console.log(`API generated ${randomPin}`);
+  res.send({ pin: randomPin });
+});
+
+app.get('/joinGame/:pin', (req: Request, res: Response) => {
+  const pin = req.params.pin;
+  console.log(`UI sent ${pin}`);
+  res.send({});
 });
 
 app.listen(port, () => {
