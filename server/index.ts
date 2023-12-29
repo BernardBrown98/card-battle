b2e30997c094e32e5003f50ad7353fd498874c25
ChrisLubin <chrisx700@gmail.com>
2023-12-28 23:11:29 -0500

    Added simple API validation for joining a game

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,11 @@
 import cors from 'cors';
 import express, { Express, Request, Response } from 'express';
-import { generateRandomDigit } from './utilities';
+import { HttpStatusCodes, generateRandomDigit } from './utilities';
 
 const app: Express = express();
 const port = 3000;
 
-let corsOptions = {
+const corsOptions = {
   origin: ['http://localhost:5173'],
 };
 
@@ -13,19 +13,35 @@ app.use(cors(corsOptions));
 
 app.get('/test', (req: Request, res: Response) => {
   console.log('ENDPOINT IS REACHED');
-  res.send({ msg: 'This is the beginning of the card battle' });
+  res.json({ msg: 'This is the beginning of the card battle' });
 });
 
 app.get('/hostGame', (req: Request, res: Response) => {
   const randomPin = `${generateRandomDigit()}${generateRandomDigit()}${generateRandomDigit()}${generateRandomDigit()}`;
   console.log(`API generated ${randomPin}`);
-  res.send({ pin: randomPin });
+  res.json({ pin: randomPin });
 });
 
 app.get('/joinGame/:pin', (req: Request, res: Response) => {
-  const pin = req.params.pin;
+  const { pin } = req.params;
   console.log(`UI sent ${pin}`);
-  res.send({});
+
+  const validPinLength = 4;
+  if (pin.length !== validPinLength) {
+    console.log('Invalid pin length');
+    res.status(HttpStatusCodes.BAD_REQUEST).json('Invalid pin length');
+    return;
+  }
+
+  const numbersOnlyRegex = /^\d+$/;
+  const isNumbersOnly = numbersOnlyRegex.test(pin);
+  if (!isNumbersOnly) {
+    console.log('Invalid pin characters');
+    res.status(HttpStatusCodes.BAD_REQUEST).json('Invalid pin characters');
+    return;
+  }
+
+  res.send({ msg: "You've joined a game!" });
 });
 
 app.listen(port, () => {
