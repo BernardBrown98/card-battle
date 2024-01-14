f047face9fed9830e32deed4e71629d05246723a
ChrisLubin <chrisx700@gmail.com>
2024-01-14 00:51:44 -0500

    Remove unused comment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,8 @@
 import { createTheme, ThemeProvider } from '@mui/material';
-import { Sockets } from './components/SocketsTest/Sockets';
+import { Sockets } from './components';
 import { Home } from './pages/Home/Home';
 
 function App() {
-  // const [data, setData] = useState<Data>({});
-  // useEffect(() => {
-  //   fetch("http://localhost:3000/test")
-  //     .then((res) => {
-  //       console.log(res);
-  //       return res.json();
-  //     })
-  //     .then((data) => setData(data));
-  // }, []);
-
   const theme = createTheme({
     typography: { h1: { fontSize: '125px' }, h2: { fontSize: '90px' }, fontFamily: ['VT323', 'monospace'].join(',') },
     palette: { background: { default: 'darkgrey' } },
